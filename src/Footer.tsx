8540d728bc51b278b376178b5dbdc5ec3ac5e631
DACKYLAM <107110497+DACKYLAM@users.noreply.github.com>
2026-07-02 13:01:11 +0200

    Fix footer copyright text formatting

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     <footer>
       <img src="/DQ_Logo-nobg.png" alt="DQ Logo" width="64px" height="64px" />
       <span>
-        © 2026 <span className="font-semibold">DataQuality Tool</span> <br/> sviluppato da Biomeris
+        © 2026 <span className="font-semibold">DataQuality Tool</span> <br/>
       </span>
       <div style={{ width: "64px", height: "64px" }}></div>
     </footer>
